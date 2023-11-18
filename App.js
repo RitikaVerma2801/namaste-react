852bf35bb95d0c952f3e5338d19d8bd930e4394c
RitikaVerma213 <108358405+RitikaVerma213@users.noreply.github.com>
2023-11-18 22:28:03 +0530

    Episode-01

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 /* <div id="parent">
        <div id="child1">
          <h1>I'm h1 tag</h1>
-         <h2>I'm h1 tag</h2>
+         <h2>I'm h2 tag</h2>
       </div>
       <div id="child2">
             <h1>I'm h1 tag</h1>
-            <h2>I'm h1 tag</h2>
+            <h2>I'm h2 tag</h2>
        </div>
 </div> */
 
@@ -14,11 +14,11 @@
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
   ]),
 ]);
 
